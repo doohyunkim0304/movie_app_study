30deeddcbaf6b8d11b7c3ba7544cf640e37adec7
doohyunkim0304 <ewb.doohyun@gmail.com>
2018-12-29 18:06:02 +0900

    before connect api

--- src/App.js
+++ src/App.js
@@ -2,47 +2,54 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title:  "Matrix",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
-  },
-  {
-    title:  "Full Metel Jacket",
-    poster: "http://mblogthumb1.phinf.naver.net/data18/2007/1/6/4/full_metal_jacket_manif-inde9898.jpg?type=w210",
-  },
-  {
-    title:  "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg",
-  },
-  {
-    title:  "Star Wars",
-    poster: "http://moonhak.co.kr/home/wp-content/uploads/bookcover/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88-%EC%94%A8%EB%84%A4%EC%95%84%ED%8A%B84_%ED%91%9C1_web.jpg"
-  },
-
-]
 
 class App extends Component {
 
   state = {
-    gretting: "Hello!"
+    gretting: "Hello!",
+
   }
   
   componentDidMount(){
-    setTimeout(() => {
-        this.setState({
-          gretting: 'Hello again!'
-        })
-    },5000)
+    setTimeout( () => {
+      this.setState({
+        movies : [
+          {
+            title:  "Matrix",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
+          },
+          {
+            title:  "Full Metel Jacket",
+            poster: "http://mblogthumb1.phinf.naver.net/data18/2007/1/6/4/full_metal_jacket_manif-inde9898.jpg?type=w210",
+          },
+          {
+            title:  "Oldboy",
+            poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg",
+          },
+          {
+            title:  "Star Wars",
+            poster: "http://moonhak.co.kr/home/wp-content/uploads/bookcover/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88-%EC%94%A8%EB%84%A4%EC%95%84%ED%8A%B84_%ED%91%9C1_web.jpg"
+          },
+          {
+            title: "toy story",
+            poster : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1Zkqxn5si7iJUnaD2Qwj_UULhkqfnQQpRBo0jZkq_mVfil0fG_g"
+          }    
+        ]
+      })
+    }, 5000)
   }
 
-  render() {
+_renderMovies = () =>{
+  const movies = this.state.movies.map((movie, index) => {
+    return <Movie title={movie.title} poster={movie.poster} key={index}/>
+  })
+  return movies;
+}
+
+  render() { 
     return (
       <div className="App">
-        {this.state.gretting}
-        {movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,35 +1,59 @@
 import React, { Component } from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
-class Movie extends Component{
+// class Movie extends Component{
 
-    static propTypes = {
-        title: PropTypes.string,
-        poster:PropTypes.string
-    }
+//     static propTypes = {
+//         title: PropTypes.string,
+//         poster:PropTypes.string
+//     }
 
-    render(){
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+//     render(){
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster}/>
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         );
+//     }
 
+// }
+
+function Movie({title,poster}){
+            return (
+                <div>
+                    <MoviePoster poster={poster}/>
+                    <h1>{title}</h1>
+                </div>
+            );            
 }
 
-class MoviePoster extends Component{
+Movie.propTypes = {
+    poster: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+}
 
-    static propTypes = {
-        poster : PropTypes.string.isRequired
-    }
+// class MoviePoster extends Component{
 
-    render(){
-        return(
-            <img src={this.props.poster} />
-        )
-    }
+//     static propTypes = {
+//         poster : PropTypes.string.isRequired
+//     }
+
+//     render(){
+//         return(
+//             <img src={this.props.poster} alt="Movie Poster" />
+//         )
+//     }
+// }
+
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="Movie Poster" />
+    )
+}
+
+MoviePoster.propTypes = {
+    poster : PropTypes.string
 }
 
 export default Movie;
